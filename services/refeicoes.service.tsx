ae39d29949f21138f578faa32aec9dc90e40c1b1
luaschelb <lua.schelb@gmail.com>
2025-09-16 14:54:27 -0300

    removed some unused code

--- services/refeicoes.service.tsx
+++ services/refeicoes.service.tsx
@@ -2,15 +2,12 @@
 import { Refeicoes } from '../models/Refeicoes.model'
 import {DatabaseConnection} from '../database/database-connection'
 
-const table = "refeicoes"
 const db=DatabaseConnection.getConnection()
-
 export default class RefeicoesService {
-
     static deleteById(id: number) {
        db.transaction(
            tx => {
-               tx.executeSql(`delete from ${table} where id = ?;`, [id], (_, { rows }) => {
+               tx.executeSql(`delete from refeicoes where id = ?;`, [id], (_, { rows }) => {
                }), (sqlError : any) => {
                    console.log(sqlError);
                }}, (txError) => {
@@ -19,10 +16,10 @@ export default class RefeicoesService {
            });
    }
    
-     static addData(param: Refeicoes) {
+    static addData(param: Refeicoes) {
         return new Promise((resolve, reject) =>db.transaction(
             tx => {
-                tx.executeSql(`insert into ${table} (nome, calorias, created_at, tipo ) 
+                tx.executeSql(`insert into refeicoes (nome, calorias, created_at, tipo ) 
                 values (?, ?, ?, ?)`, 
                 [param.nome, param.calorias, param.created_at, param.tipo], 
                 (_, { insertId, rows }) => {
@@ -37,7 +34,7 @@ export default class RefeicoesService {
 
     static findAll() {
       return new Promise((resolve, reject) => db.transaction(tx => {
-          tx.executeSql(`select * from ${table}`, [], (_, { rows }) => {
+          tx.executeSql(`select * from refeicoes`, [], (_, { rows }) => {
               resolve(rows)
           }), (sqlError : any) => {
                 console.log(sqlError);
@@ -45,41 +42,10 @@ export default class RefeicoesService {
                 console.log(txError);
         }))
     }   
-    static getByToday() {
-      return new Promise((resolve, reject) => db.transaction(tx => {
-          tx.executeSql(`select * from ${table} WHERE tipo=1`, [], (_, { rows }) => {
-              resolve(rows)
-          }), (sqlError : any) => {
-                console.log(sqlError);
-          }}, (txError) => {
-                console.log(txError);
-        }))
-    }   
-    static getByToday2() {
-        const today = new Date().toISOString().split('T')[0]; // Obtém a data atual no formato 'yyyy-mm-dd'
-        console.log(today)
-        return new Promise((resolve, reject) =>
-        db.transaction(
-            tx => { tx.executeSql(
-                `SELECT tipo FROM ${table}`,
-                [today],
-                (_, { rows }) => {
-                resolve(rows);
-                }),
-                (sqlError: any) => {
-                    console.log(sqlError);
-                }
-            },
-            txError => {
-                console.log(txError);
-            }
-        )
-        );
-    }
-    
-     static findById(id: number) {
+
+    static findById(id: number) {
         return new Promise((resolve, reject) => db.transaction(tx => {
-            tx.executeSql(`select * from ${table} where id=?`, [id], (_, { rows }) => {
+            tx.executeSql(`select * from refeicoes where id=?`, [id], (_, { rows }) => {
                 resolve(rows)
             }), (sqlError : any) => {
                 console.log(sqlError);
